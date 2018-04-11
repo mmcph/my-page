4f114f5e81dbaca45123806f785ee46bdd9be4de
Marlon McPherson <mmcpherson5@cnm.edu>
2018-04-11 14:04:04 -0600

    added project files to repo

---
+++ script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+
